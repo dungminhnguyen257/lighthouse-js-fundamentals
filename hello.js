9da6cd1498e9f8cf60f8bd35f33d46fd4726061a
dungminhnguyen257 <bryan225577@gmail.com>
2021-11-12 21:23:09 +0000

    sayHello

---
+++ hello.js
@@ -0,0 +1,14 @@
+/// First example 
+/*const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();*/
+
+/// Second example
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
